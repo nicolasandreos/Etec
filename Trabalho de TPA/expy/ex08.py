bfea5ea24e7234a85255649f5a58481ca7239592
Nicolas <nicolasandreose@gmail.com>
2022-06-02 09:14:04 -0300

    swsw
    swsw

--- Trabalho de TPA/expy/ex08.py
+++ Trabalho de TPA/expy/ex08.py
@@ -1,29 +1,26 @@
-preço=input("Digite o preço do produto: ")
+preço=float(input("Digite o preço do produto: "))
 quantidade=int(input("Digite a quantidade do produto: "))
-
 valordacompra= preço*quantidade
 
-codigo=input("Digite o codigo do pagamento")
+codigo=int(input("Digite o código do tipo do pagamento: "))
 
-if codigo > 0:
-    if codigo > 1:
-        if codigo > 2:
-            if codigo > 3:
-                valorfinal=valordacompra
-                desconto=0
-            else:
-                valorfinal1= 5% valordacompra
-                desconto1=5
-        else:
-            valorfinal2=10% valordacompra
-            desconto2=10
-    else:
-        valorfinal3= 20% valordacompra
-        desconto3=20
+if codigo == 0:
+    valorfinal= valordacompra-(25% valordacompra)
+    print("O valor da compra é de R$",valordacompra,"o valor de desconto é de 25%, o valor final é de",valorfinal,"você escolheu o pagamento a vista.")
 else:
-    valorfinal4=25% valordacompra
-    desconto4=25
-    
-print("O valor da compra é de R$",valordacompra,"com desconto de",desconto,", totalizando",valorfinal)
-
+    if codigo == 1:
+        valorfinal= valordacompra-(20% valordacompra)
+        print("O valor da compra é de R$",valordacompra,"o valor de desconto é de 20%, o valor final é de",valorfinal,"você escolheu o pagamento a cheque(30).")
+    else:       
+        if codigo == 2:
+            valorfinal= valordacompra-(10% valordacompra)
+            print("O valor da compra é de R$",valordacompra,"o valor de desconto é de 10%, o valor final é de",valorfinal,"você escolheu o pagamento cartão de crédito em 2 vezes.")
+        else:     
+            if codigo == 3:
+                valorfinal= valordacompra-(5% valordacompra)
+                print("O valor da compra é de R$",valordacompra,"o valor de desconto é de 5%, o valor final é de",valorfinal,"você escolheu o pagamento cartão de crédito em 3 vezes.")
+            else:
+                
+                if codigo == 4:
+                   print("O valor da compra é de R$",valordacompra,"o valor de desconto é de 0%,você escolheu o pagamento a vista.")
 input("enter")
